11d166be26f34df75910ea8759c3a751573138f4
AkashPaul923 <akashpaul923@gmail.com>
2026-01-01 23:38:00 +0600

    codeforces 900 rating problem

---
+++ CodeForces-Problem/B_Tab_Closing.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+
+void solve()
+{
+    ll a, b, n;
+    cin >> a >> b >> n;
+    if (a >= (b * n) || a == b)
+        cout << 1 << nl;
+    else
+        cout << 2 << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
