00bb214329943eb9e01064aaaccdf7288773d98b
AkashPaul923 <akashpaul923@gmail.com>
2026-01-02 11:39:28 +0600

    week-9 R no problem solved

---
+++ Newbie Solvers Club/Week-9/R_Max_Out.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    set<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.insert(x);
+    }
+    if (st.size() < 2)
+    {
+        cout << 0 << nl;
+        return;
+    }
+    auto end = st.end();
+    int x1 = *--end;
+    int x2 = *--end;
+    cout << x2 % x1 << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
