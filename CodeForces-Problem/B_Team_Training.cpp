c28ac4132b68b1a47768f9c8415ebaadb1c0f2b1
AkashPaul923 <akashpaul923@gmail.com>
2026-01-11 22:35:21 +0600

    codeforces problem

---
+++ CodeForces-Problem/B_Team_Training.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.rbegin(), a.rend());
+    int t = 0, m = 0;
+    for (auto s : a)
+    {
+        m++;
+        if (m * s >= x)
+        {
+            t++;
+            m = 0;
+        }
+    }
+    cout << t << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
