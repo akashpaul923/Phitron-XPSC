75a0dcae6b41e30ac2211ad8c69d746da28726b8
Akash Paul <akashpaul923@gmail.com>
2026-01-20 22:05:59 +0600

    week-2 P, B no question solved

---
+++ Super Solvers Club/Week-2/B_Divide_and_Equalize.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    map<int, int> cnt;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 2; j * j <= a[i]; j++)
+        {
+            if (a[i] % j == 0)
+            {
+                while (a[i] % j == 0)
+                {
+                    cnt[j]++;
+                    a[i] /= j;
+                }
+            }
+        }
+        if (a[i] > 1)
+        {
+            cnt[a[i]]++;
+        }
+    }
+    for (auto [k, v] : cnt)
+    {
+        if (v % n != 0)
+        {
+            no;
+            return;
+        }
+    }
+    yes;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ Super Solvers Club/Week-2/P_Exponentiation.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int x, n;
+    cin >> x >> n;
+    int ans = 1 % MOD;
+    while (n)
+    {
+        if (n & 1)
+        {
+            ans = (1LL * ans % MOD * x % MOD) % MOD;
+        }
+        x = 1LL * x * x % MOD;
+        n >>= 1;
+    }
+    cout << ans << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
