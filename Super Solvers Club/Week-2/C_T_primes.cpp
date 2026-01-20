686043e7093e1203b0a1664ace6f419f9f259313
Akash Paul <akashpaul923@gmail.com>
2026-01-20 23:36:24 +0600

    week-2 C no question solved

---
+++ Super Solvers Club/Week-2/C_T_primes.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+bool isPrime(ll n)
+{
+    if (n == 1)
+        return false;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isSquare(ll n)
+{
+    ll x = sqrtl(n);
+    return x * x == n;
+}
+
+void solve()
+{
+    ll x;
+    cin >> x;
+    if (isSquare(x) && isPrime(sqrtl(x)))
+        yes;
+    else
+        no;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
